7013c7de1b8224045c3003bdfaa3af036127bcc3
paras1612 <44598977+paras1612@users.noreply.github.com>
2019-09-23 01:59:25 +0530

    assets incomplete

---
+++ Assets/PlayerController2.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(PlayerMotor2))]
+public class PlayerController2 : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+    [SerializeField]
+    private float thruster = 1000f;
+    private PlayerMotor2 motor;
+
+    // Use this for initialization
+    void Start()
+    {
+        print("Started Controller");
+        motor = GetComponent<PlayerMotor2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+        //  print(_xMov);
+        // print(_zMov);
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+        //Move
+        motor.Move(_velocity);
+        //turn camera horizontally
+        float _yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+        motor.Rotate(_rotation);
+        //turn camera vertically
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+        motor.RotateCamera(_cameraRotation);
+
+        //jump
+
+        Vector3 _thrusterforce = Vector3.zero;
+        if (Input.GetButton("Jump"))
+        {
+            _thrusterforce = Vector3.up * thruster;
+        }
+        motor.ApplyThruster(_thrusterforce);
+    }
+}
---
+++ Assets/PlayerMotor2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor2 : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+    private Vector3 thrusterForce = Vector3.zero;
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        print("Started Motor");
+        rb = GetComponent<Rigidbody>();
+    }
+    public void Move(Vector3 _velocity)
+    {
+        velocity =- _velocity;
+    }
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+    public void ApplyThruster(Vector3 _thruster)
+    {
+        thrusterForce = _thruster;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+        if (thrusterForce != Vector3.zero)
+        {
+            rb.AddForce((thrusterForce * Time.fixedDeltaTime), ForceMode.Acceleration);
+        }
+    }
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            //print("Rotating");
+            cam.transform.Rotate(-cameraRotation);
+        }
+    }
+
+}
